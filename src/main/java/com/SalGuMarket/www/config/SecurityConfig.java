845d5e8fa3e67bed9ab65cb261895b2bc450aa71
JaeYong-An <ax5388@naver.com>
2024-03-19 15:11:11 +0900

    merge

--- src/main/java/com/SalGuMarket/www/config/SecurityConfig.java
+++ src/main/java/com/SalGuMarket/www/config/SecurityConfig.java
@@ -9,10 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
